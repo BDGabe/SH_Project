974d0001137a1eaaf99f9220fc44858a9439cfe7
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-21 12:45:00 -0400

    Using NodeJS and Express. Using GCP Map API and API key.

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -1,29 +1,13 @@
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
+var express = require('express')
+var app = express()
 
-// Configure the HTTP server to print Hello World! to all requests.
-var server = http.createServer(function (request, response) {
-    if (req.url === "/create") {
-        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
+app.use(express.static('www'));
 
-            resp.end();
-        });
-    } else {
-        //4.
-        resp.writeHead(200, { 'Content-Type': 'text/html' });
-        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
-        resp.end();
-});
+var server = app.listen(8000, function () {
 
-// Listen on port 8080
-server.listen(8080);
+    var host = server.address().address
+    var port = server.address().port
 
-console.log('Server available at http://localhost:8080/');
+    console.log('Express app listening at http://%s:%s', host, port)
+
+})
