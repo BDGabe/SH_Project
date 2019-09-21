7abe6c05368b5c32925c39c4027e057c2b773f0c
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-21 12:20:25 -0400

    Read and write html page through JS script

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -1,10 +1,26 @@
 // Load the http module to create an http server.
 var http = require('http');
+var fs = require('fs');
 
 // Configure the HTTP server to print Hello World! to all requests.
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World!\n");
+    if (req.url === "/create") {
+        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
 });
 
 // Listen on port 8080
