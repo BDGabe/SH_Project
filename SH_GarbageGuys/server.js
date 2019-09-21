125d9716d88badf8b2a86f9a651e7cc7ecb3321d
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-21 11:52:03 -0400

    Initial blank

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -1,8 +1,13 @@
-'use strict';
+// Load the http module to create an http server.
 var http = require('http');
-var port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+// Configure the HTTP server to print Hello World! to all requests.
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World!\n");
+});
+
+// Listen on port 8080
+server.listen(8080);
+
+console.log('Server available at http://localhost:8080/');
