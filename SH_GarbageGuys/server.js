426b6abfa23fc4f268a5d7a7241fe1e189a45492
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-22 00:30:58 -0400

    General script and server changes and comments

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -3,11 +3,34 @@ var app = express()
 
 app.use(express.static('www'));
 
+/*const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'http://localhost:8000',
+    user: 'root',
+    password: 'Lopez4464',
+    connectionLimit: 5
+});
+
+async function asyncFunction() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT 1 as val");
+        console.log(rows); //[ {val: 1}, meta: ... ]
+        const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
+}*/
+
 var server = app.listen(8000, function () {
 
     var host = server.address().address
     var port = server.address().port
 
     console.log('Express app listening at http://%s:%s', host, port)
-
 })
--- SH_GarbageGuys/www/script.js
+++ SH_GarbageGuys/www/script.js
@@ -1,10 +1,47 @@
 // Initialize and add the map
+/*function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: { lat: -34.397, lng: 150.644 }
+    });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
+}
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}*/
+
 function initMap()
 {
     // The location of Uluru
     var uluru = {lat: 25.7574, lng: -80.3733};
     // The map, centered at Uluru
-    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: uluru});
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+    var image = {
+        url: "./recycleFav.png",
+        scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        draggable: true,
+        icon: image,
+        title: 'Hello World!'
+    });
 }
