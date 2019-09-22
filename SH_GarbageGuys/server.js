be80ced11c6cc8f8b09ef87eb33b7c1f98113b08
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-22 02:12:23 -0400

    Changed icons

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -1,31 +1,15 @@
-var express = require('express')
+var express = require('express');
+//var mysql = require('mysql');   
 var app = express()
 
 app.use(express.static('www'));
 
-/*const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-    host: 'http://localhost:8000',
+/*var connection = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: 'Lopez4464',
-    connectionLimit: 5
-});
-
-async function asyncFunction() {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query("SELECT 1 as val");
-        console.log(rows); //[ {val: 1}, meta: ... ]
-        const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-
-    } catch (err) {
-        throw err;
-    } finally {
-        if (conn) return conn.end();
-    }
-}*/
+    database: 
+})*/
 
 var server = app.listen(8000, function () {
 
