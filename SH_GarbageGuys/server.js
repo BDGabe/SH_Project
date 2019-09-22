a48432d2660aae5b8c241d12d3a05d21c9078d2d
BDGabe <42942667+BDGabe@users.noreply.github.com>
2019-09-22 02:39:10 -0400

    Database stuff

--- SH_GarbageGuys/server.js
+++ SH_GarbageGuys/server.js
@@ -4,14 +4,26 @@ var app = express()
 
 app.use(express.static('www'));
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Lopez4464',
-    database: 
-})*/
+    database: 'SHGG:test:1' 
+})
+
+connection.connect(function (error) {
+    if (!!error) {
+        console.log('Error');
+    }
+    else {
+        console.log('Connected')
+    }
+})
+
+app.get
 
 var server = app.listen(8000, function () {
+    connection.query("SELECT * FROM SHGG:test:1", function (error, rows, fields) { });
 
     var host = server.address().address
     var port = server.address().port
