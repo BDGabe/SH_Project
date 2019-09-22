6d066f78645aec1603bfaab5a1e68dd7b4080edd
goodpaulo <plastra2@gmail.com>
2019-09-22 00:05:19 -0400

    Added draggable marker that will delete once a new one is made.

--- SH_GarbageGuys/www/script.js
+++ SH_GarbageGuys/www/script.js
@@ -1,10 +1,42 @@
+var markers = [];
+
 // Initialize and add the map
-function initMap()
-{
-    // The location of Uluru
-    var uluru = {lat: 25.7574, lng: -80.3733};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+function initMap() {
+    var location = { lat: 25.7574, lng: -80.3733 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: location
+    });
+
+    map.addListener('click', function (e) {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
+}
+
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++)
+    {
+        markers[i].setMap(map);
+    }
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+    var image = {
+        url: "./recycleFav.png",
+        scaledSize: new google.maps.Size(25, 25)
+    };
+
+    setMapOnAll(null);
+    markers = [];
+
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: true,
+        icon: image,
+        title: 'Location'
+    });
+
+    markers.push(marker);
+    map.panTo(latLng);
 }
